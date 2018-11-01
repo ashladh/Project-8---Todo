d14ed0fb8d3761351a979f1e3fcd2c31bea1d083
Ash <lou.mosnier@gmail.com>
2018-11-01 18:25:24 +0100

    Update store specs

--- test/StoreSpec.js
+++ test/StoreSpec.js
@@ -1,7 +1,11 @@
 describe('store', function() {
     var storage
+    var currentId = 0
+
     beforeEach(function() {
+        currentId = 0
         delete localStorage.storeSpecs
+        delete localStorage.storeSpecs_lastId
         storage = new app.Store('storeSpecs')
     })
 
@@ -12,10 +16,8 @@ describe('store', function() {
         expect('storeTest' in localStorage).toBe(true)
     })
 
-    var currentId = 0
-
     function addTodo(todo) {
-        todo = todo || {title: 'my todo', completed: false, id: currentId++}
+        todo = todo || {title: 'my todo' + currentId, completed: false, id: currentId++}
         var storing = JSON.parse(localStorage.storeSpecs)
         storing.todos.push(todo)
         localStorage.storeSpecs = JSON.stringify(storing)
@@ -44,5 +46,53 @@ describe('store', function() {
         })
 
         expect(foundTodos.length).toEqual(3)
+    })
+
+    it ('should get the last id', function() {
+        expect(storage.getLastId()).toEqual(0)
+        localStorage.storeSpecs_lastId = 1
+        expect(storage.getLastId()).toEqual(1)
+    })
+
+    it ('should create the next id', function() {
+        localStorage.storeSpecs_lastId = 1
+        expect(storage.createNextId()).toEqual(2)
+    })
+
+    it ('should find an item based on a query', function() {
+        addTodos(3)
+        var foundItem
+        storage.find({title: 'my todo2'}, function(items) {
+            foundItem = items[0]
+        })
+        expect(foundItem.title).toEqual('my todo2')
+    })
+
+    it ('should save a new item', function() {
+        storage.save({title: 'my new todo', completed: true})
+        var items = JSON.parse(localStorage.storeSpecs).todos
+        expect(items.length).toEqual(1)
+        expect(items[0].title).toEqual('my new todo')
+        expect(items[0].completed).toBe(true)
+        expect(items[0].id).toEqual(1)
+    })
+
+    it ('should update an item', function() {
+        storage.save({title: 'my new todo', completed: true})
+        storage.save({title: 'my updated todo'}, null, 1)
+        var items = JSON.parse(localStorage.storeSpecs).todos
+        expect(items.length).toEqual(1)
+        expect(items[0].title).toEqual('my updated todo')
+        expect(items[0].id).toEqual(1)
+    })
+
+    it ('should remove an item', function() {
+        addTodos(3)
+        storage.remove(1, function() {})
+        var items = JSON.parse(localStorage.storeSpecs).todos
+        expect(items.length).toEqual(2)
+        for (var i = 0; i < items.length; i++) {
+            expect(items[i].id).not.toEqual(1)
+        }
     })
 })
---
+++ test/TemplateSpec.js
@@ -0,0 +1,36 @@
+describe('template', function() {
+    var template
+    beforeEach(function() {
+        template = new app.Template()
+    })
+
+    it ('should have a default template', function() {
+        expect(template.defaultTemplate).toEqual('<li data-id="{{id}}" class="{{completed}}"><div class="view"><input class="toggle" type="checkbox" {{checked}}><label>{{title}}</label><button class="destroy"></button></div></li>')
+    })
+
+    it ('should render the template with items', function() {
+        var view = template.show([{
+            title: 'my todo',
+            id: '1',
+            completed: false
+        }, {
+            title: 'my todo2',
+            id: '2',
+            completed: true
+        }])
+        var viewItem1 = '<li data-id="1" class=""><div class="view"><input class="toggle" type="checkbox" ><label>my todo</label><button class="destroy"></button></div></li>'
+        var viewItem2 = '<li data-id="2" class="completed"><div class="view"><input class="toggle" type="checkbox" checked><label>my todo2</label><button class="destroy"></button></div></li>'
+
+        expect(view).toEqual(viewItem1 + viewItem2)
+    })
+
+    it ('should render the number of active todos', function() {
+        expect(template.itemCounter(3)).toEqual('<strong>3</strong> items left')
+        expect(template.itemCounter(1)).toEqual('<strong>1</strong> item left')
+    })
+
+    it ('should render "clear completed" button text', function() {
+        expect(template.clearCompletedButton(0)).toEqual('')
+        expect(template.clearCompletedButton(3)).toEqual('Clear completed')
+    })
+})
